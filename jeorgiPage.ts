5dd57bfbd3f2d7a7737f6e07fa3840bf84e5f397
Jeorgi Bernard <Jeorgi.Bernard@gmail.com>
2023-12-06 19:09:52 -0700

    jeorgis branch first push

---
+++ jeorgiPage.ts
@@ -0,0 +1,8 @@
+import {By} from 'selenium-webdriver'; 
+import {BasePage} from './basePage'; 
+
+export class Mars extends BasePage {
+    //! Locators go here; 
+    //!constructor
+    //!methods
+}; 
---
+++ jeorgiTest.test.ts
@@ -0,0 +1,6 @@
+import { Mars } from "./marsPage";
+const mars = new Mars(); 
+
+test("", async () => {
+
+}); 
