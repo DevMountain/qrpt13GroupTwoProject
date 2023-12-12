eb4c28eb1605c269a95deac8ceb2f7a3ff174e61
Jeorgi Bernard <Jeorgi.Bernard@gmail.com>
2023-12-11 20:20:18 -0700

    Stats Page testing

--- jeorgiPage.ts
+++ jeorgiPage.ts
@@ -4,7 +4,19 @@ import {BasePage} from './basePage';
 export class statsNFL extends BasePage {
     //! Locators go here; 
      player: By = By.xpath('(//a[@class="d3-o-player-fullname nfl-o-cta--link"])[1]');
-     pResult: By = By.id('main-content');
+     result: By = By.id('main-content');
+     rush: By = By.xpath('//a[text()="Rushing"]');
+     team: By = By.xpath('(//span[@class="d3-o-nav__item-label"])[63]');
+     cookies: By = By.id('onetrust-close-btn-container');
+     pStats: By = By.xpath('(//span[@class="d3-o-nav__item-label"])[62]')
+
     //!constructor
+    constructor() {
+        super({url:'https://www.nfl.com/stats/player-stats/'})
+    };
     //!methods
+
+    async getResults() {
+        return this.getText(this.result)
+    };
 }; 
--- jeorgiTest.test.ts
+++ jeorgiTest.test.ts
@@ -1,6 +1,47 @@
 import { statsNFL } from "./jeorgiPage"; 
 const stats = new statsNFL(); 
+const fs = require('fs');
 
-test("", async () => {
+test("click buttons on stats page", async () => {
+    await stats.navigate();
+    await stats.driver.manage().window().maximize();
+    await stats.click(stats.cookies);
+    await stats.click(stats.rush);
+    let rushText = await stats.getResults();
+    expect(rushText).toContain("2023 NFL rushing stats");
+
+    await fs.writeFile(`${__dirname}/rushing.png`,
+    await stats.driver.takeScreenshot(), 'base64',
+    (e)=>{
+        if (e) console.error(e)
+        else console.log('You did it')
+    });
+
+    await stats.click(stats.team);
+    let teamText = await stats.getResults();
+    expect(teamText).toContain("NFL 2023 REG");
+
+    await fs.writeFile(`${__dirname}/team.png`,
+    await stats.driver.takeScreenshot(), 'base64',
+    (e)=>{
+        if (e) console.error(e)
+        else console.log('You did it')
+    });
+
+    await stats.click(stats.pStats);
+    await stats.click(stats.player);
+    let playerText = await stats.getResults();
+    expect(playerText).toContain("C.J. Stroud");
+
+    await fs.writeFile(`${__dirname}/player.png`,
+    await stats.driver.takeScreenshot(), 'base64',
+    (e)=>{
+        if (e) console.error(e)
+        else console.log('You did it')
+    });
+
+
+    await stats.driver.quit();
+
 
 }); 
